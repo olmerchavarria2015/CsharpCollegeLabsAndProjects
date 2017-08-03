663c8cf6d375551609a5257fef9afb0c7918ebf3
Alex Chavarria <olmerchavarria@gmail.com>
2017-08-02 22:52:47 -0500

    Add files via upload

---
+++ Income Tax Calculator/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Income_Tax
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.totalIncomeTBox = new System.Windows.Forms.TextBox();
+            this.dependentsTBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.goiOutputLabel = new System.Windows.Forms.Label();
+            this.incomeTaxOutputLabel = new System.Windows.Forms.Label();
+            this.maritalStatusCheckBox = new System.Windows.Forms.CheckBox();
+            this.marrieLabel = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tax Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total Income";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Dependents";
+            // 
+            // totalIncomeTBox
+            // 
+            this.totalIncomeTBox.Location = new System.Drawing.Point(239, 80);
+            this.totalIncomeTBox.Name = "totalIncomeTBox";
+            this.totalIncomeTBox.Size = new System.Drawing.Size(100, 20);
+            this.totalIncomeTBox.TabIndex = 3;
+            // 
+            // dependentsTBox
+            // 
+            this.dependentsTBox.Location = new System.Drawing.Point(239, 122);
+            this.dependentsTBox.Name = "dependentsTBox";
+            this.dependentsTBox.Size = new System.Drawing.Size(60, 20);
+            this.dependentsTBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(139, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Compute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Gross Adjusted Income";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(27, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Income Tax";
+            // 
+            // goiOutputLabel
+            // 
+            this.goiOutputLabel.AutoSize = true;
+            this.goiOutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goiOutputLabel.Location = new System.Drawing.Point(236, 212);
+            this.goiOutputLabel.Name = "goiOutputLabel";
+            this.goiOutputLabel.Size = new System.Drawing.Size(47, 15);
+            this.goiOutputLabel.TabIndex = 8;
+            this.goiOutputLabel.Text = "label6";
+            // 
+            // incomeTaxOutputLabel
+            // 
+            this.incomeTaxOutputLabel.AutoSize = true;
+            this.incomeTaxOutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.incomeTaxOutputLabel.Location = new System.Drawing.Point(236, 261);
+            this.incomeTaxOutputLabel.Name = "incomeTaxOutputLabel";
+            this.incomeTaxOutputLabel.Size = new System.Drawing.Size(47, 15);
+            this.incomeTaxOutputLabel.TabIndex = 9;
+            this.incomeTaxOutputLabel.Text = "label7";
+            // 
+            // maritalStatusCheckBox
+            // 
+            this.maritalStatusCheckBox.AutoSize = true;
+            this.maritalStatusCheckBox.Location = new System.Drawing.Point(28, 43);
+            this.maritalStatusCheckBox.Name = "maritalStatusCheckBox";
+            this.maritalStatusCheckBox.Size = new System.Drawing.Size(102, 17);
+            this.maritalStatusCheckBox.TabIndex = 10;
+            this.maritalStatusCheckBox.Text = "Check if married";
+            this.maritalStatusCheckBox.UseVisualStyleBackColor = true;
+            this.maritalStatusCheckBox.CheckedChanged += new System.EventHandler(this.maritalStatusCheckBox_CheckedChanged);
+            // 
+            // marrieLabel
+            // 
+            this.marrieLabel.AutoSize = true;
+            this.marrieLabel.Location = new System.Drawing.Point(221, 64);
+            this.marrieLabel.Name = "marrieLabel";
+            this.marrieLabel.Size = new System.Drawing.Size(134, 13);
+            this.marrieLabel.TabIndex = 11;
+            this.marrieLabel.Text = "Enter total couple\'s income";
+            this.marrieLabel.Visible = false;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(128, 309);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorLabel.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 334);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.marrieLabel);
+            this.Controls.Add(this.maritalStatusCheckBox);
+            this.Controls.Add(this.incomeTaxOutputLabel);
+            this.Controls.Add(this.goiOutputLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dependentsTBox);
+            this.Controls.Add(this.totalIncomeTBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tax Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox totalIncomeTBox;
+        private System.Windows.Forms.TextBox dependentsTBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label goiOutputLabel;
+        private System.Windows.Forms.Label incomeTaxOutputLabel;
+        private System.Windows.Forms.CheckBox maritalStatusCheckBox;
+        private System.Windows.Forms.Label marrieLabel;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
+
---
+++ Income Tax Calculator/Form1.cs
@@ -0,0 +1,171 @@
+﻿//Filename: TaxCalculator
+//Written by: Olmer Chavarria
+//10-02-2016
+//I have included and extra array with the rates for tax as married,
+// also an extra method for calculating tax for married couples
+//  to earn 10 extrapoints.
+using System;
+using System.Windows.Forms;
+
+namespace Income_Tax
+{
+    public partial class Form1 : Form
+    {
+        
+
+
+        double[,] singleArray =
+            { { .10, .15, .25, .28, .33, .35, .396 },
+            { 0.0, 9275.0, 37650.0, 91150.0, 190150.0, 413350.0, 415050.0 },
+            { 0.0, 927.50, 5183.75, 18558.75, 46278.75, 119934.75, 120529.75 }
+        };
+        double[,] marriedArray =
+            { { .10, .15, .25, .28, .33, .35, .396 },
+            { 0.0, 18550.0, 75300.0, 151900.0, 231450.0, 413350.0, 466950.0 },
+            { 0.0, 1855.0, 10367.50, 29517.50, 51791.50, 111818.50, 130578.50 }
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void calculateMarriedTax() //calculates married couples income tax
+        {
+            double standardDeduction = 6300;
+            double exCeption = 4050;
+            double totalIncome = 0;
+            double adjustedGrossIncome = 0;
+            double dependents = 2;                 //number of the dependents for wife and husband
+            double taxableIncomeRate = 0;
+            double taxDueToIncome = 0;
+            double incomeThreshold = 0;
+            double incomeTax = 0;
+            try
+            {
+                if (!(errorLabel.Text.Length == 0)) { errorLabel.Text = ""; }
+
+                dependents = dependents + double.Parse(dependentsTBox.Text); //add dependents other than self
+                totalIncome = double.Parse(totalIncomeTBox.Text);
+                adjustedGrossIncome = totalIncome - (exCeption * dependents) - standardDeduction;
+                if (adjustedGrossIncome > 0)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        if (adjustedGrossIncome >= marriedArray[1, i])//adjustedGrossIncome is used here to find the other rates
+                        {
+                            taxableIncomeRate = marriedArray[0, i];
+                            incomeThreshold = marriedArray[1, i];
+                            taxDueToIncome = marriedArray[2, i];
+                        }
+                        else { continue; }
+                    }
+                    incomeTax = taxDueToIncome + (adjustedGrossIncome - incomeThreshold) * taxableIncomeRate;
+                    goiOutputLabel.Text = adjustedGrossIncome.ToString("c2");
+                    incomeTaxOutputLabel.Text = incomeTax.ToString("c2");
+
+                }
+                else
+                {
+                    incomeTax += taxDueToIncome + (totalIncome - incomeThreshold) * taxableIncomeRate;
+                    goiOutputLabel.Text = adjustedGrossIncome.ToString("c2");
+                    incomeTaxOutputLabel.Text = "$0.00";
+
+                }
+
+
+
+
+
+            }
+            catch
+            {
+                errorLabel.Text = "ERROR ON INPUT";
+                //throw;
+            }
+        }
+        public void calculateTax() //calculates single's income tax
+        {
+            //formulas
+            //Compute the Adjusted Gross Income(AGI) = totalIncome - (exCeption * dependents) - standardDeduction 
+            //The tax is = taxDueToIncome + (adjustedGrossIncome - incomeThreshold) * taxableIncomeRate
+
+            double standardDeduction = 6300;
+            double exCeption = 4050;
+            double totalIncome = 0;
+            double adjustedGrossIncome = 0;
+            double dependents = 1;                  //number of dependents for self
+            double taxableIncomeRate = 0;
+            double taxDueToIncome = 0;
+            double incomeThreshold = 0;
+            double incomeTax = 0;
+            try
+            {
+                if (!(errorLabel.Text.Length == 0)) { errorLabel.Text = ""; }
+
+                dependents = dependents + double.Parse(dependentsTBox.Text); //
+                totalIncome = double.Parse(totalIncomeTBox.Text);
+                adjustedGrossIncome = totalIncome - (exCeption * dependents) - standardDeduction;
+                if (adjustedGrossIncome > 0)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        if (adjustedGrossIncome >= singleArray[1, i]) //adjustedGrossIncome is used here to find the other rates 
+                        {
+                            taxableIncomeRate = singleArray[0, i];
+                            incomeThreshold = singleArray[1, i];
+                            taxDueToIncome = singleArray[2, i];
+                        }
+                        else { continue; }
+                    }
+                    incomeTax = taxDueToIncome + (adjustedGrossIncome - incomeThreshold) * taxableIncomeRate;
+                    goiOutputLabel.Text = adjustedGrossIncome.ToString("c2");
+                    incomeTaxOutputLabel.Text = incomeTax.ToString("c2");
+
+                }
+                else
+                {
+                    incomeTax += taxDueToIncome + (totalIncome - incomeThreshold) * taxableIncomeRate;
+                    goiOutputLabel.Text = adjustedGrossIncome.ToString("c2");
+                    incomeTaxOutputLabel.Text = "$0.00";
+
+                }
+
+
+
+
+
+            }
+            catch
+            {
+                errorLabel.Text = "ERROR ON INPUT";
+                //throw;
+            }
+        }
+
+
+        private void maritalStatusCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (maritalStatusCheckBox.Checked && !marrieLabel.Visible)
+            {
+                marrieLabel.Visible = true;
+            }
+            else { marrieLabel.Visible = false; }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            if (maritalStatusCheckBox.Checked) //checks if the marital status is checked
+            {
+                calculateMarriedTax();
+            }
+            else
+            {
+
+                calculateTax();
+            }
+
+
+
+        }
+    }
+}
